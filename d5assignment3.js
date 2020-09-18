4106e3c8b6b2ec222c0c8d3c2dc65be05411b63a
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-09-18 11:33:01 -0700

    third commit

--- d5assignment3.js
+++ d5assignment3.js
@@ -3,9 +3,33 @@
  */
 
 const convertIntToBinary = function(num) {
-    let ans = num.toString(2);
-    return ans;
+    // let ans = num.toString(2);
+    // return ans;
+    let arr = [128,64,32,16,8,4,2,1];
+    let ans ='';
+    for (i = 0; i < arr.length; i++){
+        // console.log(ans);
+        if(255 >=num && num >= 0){
+            if(num < arr[i] && num < arr[i-1]){
+                ans += 0;
+            }else if(num > arr[i] && num < arr[i-1]){
+                ans += 1;
+                num -= arr[i];
+            }else if(num < arr[i]){
+                ans += 0;
+            }else if(num === arr[i]){
+                ans += 1;
+                num -= arr[i];
+            }else if(num > arr[0]){
+                ans += 1;
+                num -= arr[i];
+            }
+        }else{
+            return "Out of scale";
+        }
+    }
+    return ans; 
 
 };
-console.log(convertIntToBinary(100));
+console.log(convertIntToBinary(32));
 
