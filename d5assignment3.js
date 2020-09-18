e2a72eb138d445151569b4498e9f8e463b2a3492
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-09-18 10:57:31 -0700

    third commit

---
+++ d5assignment3.js
@@ -0,0 +1,11 @@
+/**
+ * Write me a function that converts any integer (up to 255) into binary
+ */
+
+const convertIntToBinary = function(num) {
+    let ans = num.toString(2);
+    return ans;
+
+};
+console.log(convertIntToBinary(100));
+
